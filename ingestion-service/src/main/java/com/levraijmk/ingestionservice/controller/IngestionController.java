8c4d2940459c14356af4436ad71a18f274dfc24e
Joelmk28 <joelmuhindok@gmail.com>
2026-05-16 14:25:01 +0100

    ajout de la configuration kafka dans docker-compose

---
+++ ingestion-service/src/main/java/com/levraijmk/ingestionservice/controller/IngestionController.java
@@ -0,0 +1,23 @@
+package com.levraijmk.ingestionservice.controller;
+
+import com.levraijmk.ingestionservice.dto.EnergyUsageDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/ingestion")
+public class IngestionController {
+
+    private final IngestionService ingestionService;
+
+    public IngestionController(final IngestionService ingestionService){
+        this.ingestionService = ingestionService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void ingestData(@RequestBody EnergyUsageDto usageDto){
+        ingestionService.ingestionEnergyUsage(usageDto);
+    }
+
+}
---
+++ ingestion-service/src/main/java/com/levraijmk/ingestionservice/dto/EnergyUsageDto.java
@@ -0,0 +1,16 @@
+package com.levraijmk.ingestionservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+public record EnergyUsageDto (
+    Long deviceId,
+    double energyConsumed,
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    Instant timestamp
+){}
