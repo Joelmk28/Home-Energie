18123cc6c5596aae5bbb84af3ce7a6bd4d5bf669
Joelmk28 <joelmuhindok@gmail.com>
2026-05-25 00:23:46 +0100

    ajout UsageService et InfluxDbConfig

---
+++ usage-servce/src/main/java/com/levraijmk/usageservce/service/UsageService.java
@@ -0,0 +1,55 @@
+package com.levraijmk.usageservce.service;
+
+
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.domain.WritePrecision;
+import com.influxdb.client.write.Point;
+import com.levraijmk.usageservce.kafka.event.EnergyUsageEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+
+@Service
+@Slf4j
+public class UsageService {
+
+
+    private final InfluxDBClient influxDBClient;
+
+
+    @Value("${influx.bucket}")
+    private String influxBucket;
+
+    @Value("${influx.org}")
+    private String influxOrg;
+
+    public UsageService(InfluxDBClient influxDBClient) {
+        this.influxDBClient = influxDBClient;
+    }
+
+
+    @KafkaListener(topics = "energy-usage", groupId = "usage-service")
+    public void energyUsageEvent(EnergyUsageEvent event) {
+
+        try {
+            log.info("Received energy usage event: {}", event);
+
+            Point point = Point.measurement("energy-usage")
+                    .addTag("deviceId", String.valueOf(event.deviceId()))
+                    .addField("energyConsumed", event.energyConsumed())
+                    .time(event.timestamp(), WritePrecision.MS);
+
+            influxDBClient.getWriteApiBlocking()
+                    .writePoint(influxBucket, influxOrg, point);
+
+        } catch (Exception e) {
+            log.error("Error processing Kafka message", e);
+        }
+    }
+
+
+
+}
